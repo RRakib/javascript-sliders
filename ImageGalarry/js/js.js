2365aa4b483a74214356fbb3b135d9f52a294771
Rakib <16103030@iubat.edu>
2019-09-22 23:10:46 +0600

    Added Slide Pointers

--- ImageGalarry/js/js.js
+++ ImageGalarry/js/js.js
@@ -1,28 +1,38 @@
 let state = {
-    image: 0 
+    image: 0
 }
+
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
 let elImages = Array.from(document.querySelectorAll('.ui-data-image'))
 let elThumbnail = Array.from(document.querySelectorAll('.ui-thumbnail'))
 
-const send = (event) => {
-    let elPhotoes = Array.from(document.querySelectorAll('[data-active]'))
-    elPhotoes.forEach(el => el.removeAttribute('[data-active]'))
-    
-    switch(event){
+const displaySlide = (index) => {
+    Array.from(document.querySelectorAll(`[data-active]`)).forEach(el => el.removeAttribute('data-active'))
+    switch(index){
         case 'PREV':
-            state.image = Math.min(state.image - 1, 0)
-            break;
+            console.log(state.image)
+            state.image > 0 ? state.image-- : state.image = 0
+            break
         case 'NEXT':
-            state.image = Math.max(state.image + 1, elPhotoes.length)
-            break;
+            state.image < 5 ? state.image++ : state.image = 5
+            break
         default:
-            state.image = event
-            break;
+            state.image = index
+            break
     }
     Array.from(document.querySelectorAll(`[data-key='${state.image}']`)).forEach(el => el.setAttribute('data-active', true))
 }
 
 elThumbnail.forEach(el => el.addEventListener('click', () => {
-    console.log(el.dataset.key)
-    send(el.dataset.key)
+    displaySlide(el.dataset.key)
 }))
+
+next.addEventListener('click', () => {
+    displaySlide('NEXT')
+})
+prev.addEventListener('click', () => {
+    displaySlide('PREV')
+})
+
+displaySlide(0)
